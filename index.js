5a40fc1942b37bc96fd47fafc3932b5e892519b4
shanmugapriyan03 <shanmugapriyanramasamy381@gmail.com>
2025-04-17 22:57:39 +0530

    chore: semicolon added

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 console.log("New JS file added ");
-console.log("This changes made from remote repo")
+console.log("This changes made from remote repo");
 console.log("Today i am changing the content");
